62d2e2db28842d2d449c1e7f1ddc82df24ed9064
Alex Mark <alexvmar@yandex.ru>
2023-02-13 11:14:02 +0300

    change file task_1_3

--- task_1_3/Program.cs
+++ task_1_3/Program.cs
@@ -4,4 +4,26 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите магическое число N");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a > 1)
+{
+    for (int i = 2; i <= a; i = i + 2)
+    {
+        if (a >= 4)
+        {
+            Console.Write(i + " ");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Необходимо перезапустить программу и ввести число больше 1");
+}
+
+
